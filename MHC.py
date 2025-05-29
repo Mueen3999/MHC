a5cc47233a88f85c880ed25a2c4ace04f8cb95ee
Mueen3999 <a70567071@gmail.com>
2025-05-29 12:12:32 +0800

    Update MHC.py

--- MHC.py
+++ MHC.py
@@ -8,10 +8,10 @@ import os
 os.system('git pull')
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from MHB64 import main
+    from MHC64 import main
     main()
 elif bit == '32bit':
-    from MHB32 import main
+    from MHC32 import main
     main()
 else:
     print('\n YOUR DEVICE IS NOT SUPPORT THIS TOOL')
